dead5fb728e3ad5e4ea485d5275894a6b8cc4acb
Khánh <91839948+Kirolo99@users.noreply.github.com>
2021-10-04 22:09:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ const CONFIG = {
     question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    reply: 'Tại vì em yêu anh đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng yêu em lắm 🥰. Love you 300.000 😘😘',
     messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    messLink: 'https://m.me/Nam.Khanh1332008'
 }
